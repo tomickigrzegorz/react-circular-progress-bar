8cc2c83431be5805b6fb9da60b1c0948fe85a925
Grzegorz Tomicki <gtomik@gmail.com>
2021-07-02 22:46:56 +0200

    docs: example of use

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import CircularProgressBar from './components/CircularProgressBar';
+
+const propsA = {
+  percent: 70,
+  colorSlice: "#E91E63",
+}
+
+const propsB = {
+  linearGradient: ["yellow", "#ff0000"],
+  round: true,
+  percent: 66,
+  colorSlice: "#4CAF50",
+  colorCircle: "#e6e6e6"
+}
+
+const propsC = {
+  percent: 55,
+  colorSlice: "#CDDC39",
+  colorCircle: "#f1f1f1",
+}
+
+const propsD = {
+  percent: 60,
+  colorSlice: "#000",
+  colorCircle: "#e6e6e6",
+  number: false
+}
+
+function App() {
+  const [seconds, setSeconds] = useState(propsA);
+
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+      setSeconds({
+        percent: Math.floor((Math.random() * 100) + 1),
+        colorSlice: "#E91E63"
+      });
+    }, 3000);
+
+    return () => clearInterval(interval);
+
+  }, []);
+
+  return (
+    <div className="container">
+      <CircularProgressBar {...seconds} />
+      <CircularProgressBar {...propsB} />
+      <CircularProgressBar {...propsC} />
+      <CircularProgressBar {...propsD} />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import CircularProgressBar from './components/CircularProgressBar';
-
-const props = {
-  percent: 70,
-  colorSlice: "#E91E63",
-}
-
-// const props = {
-//   round: true,
-//   percent: 70,
-//   colorSlice: "#4CAF50",
-//   colorCircle: '#e6e6e6',
-//   linearGradient: ["yellow", "#ff0000"]
-// }
-
-// const props = {
-//   percent: 55,
-//   colorSlice: "#CDDC39",
-//   colorCircle: "#f1f1f1",
-//   fontWeight: 100
-// }
-
-// without number
-// const props = {
-//   percent: 60,
-//   colorSlice: "#000",
-//   colorCircle: "#e6e6e6",
-//   number: false
-// }
+import App from './App';
 
 ReactDOM.render(
-  <CircularProgressBar {...props} />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
