7e27052bc8c4858abfb3a20ecba696726158a95f
Grzegorz Tomicki <gtomik@gmail.com>
2021-07-06 11:47:01 +0200

    docs: adding a storybook

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@storybook/addon-controls",
+    '@storybook/preset-create-react-app'
+  ]
+}
---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+
+import 'intersection-observer';
+
+export const parameters = {
+  layout: 'centered',
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    expanded: true,
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    source: {
+      excludeDecorators: true,
+    },
+  },
+};
