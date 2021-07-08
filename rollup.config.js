c7a73f5863fb052b57feeb29f699a33efe7ff59b
Grzegorz Tomicki <gtomik@gmail.com>
2021-07-08 12:07:05 +0200

    chore: rollup

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import del from 'rollup-plugin-delete';
+import pkg from './package.json';
+
+export default {
+  input: './src/components/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    del({
+      targets: 'dist/*',
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    resolve(),
+    commonjs(),
+  ],
+};
