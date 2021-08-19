862564bc3503ae9a882c0cfa3fb424c1e6ad2f79
Grzegorz Tomicki <gtomik@gmail.com>
2021-08-19 13:12:35 +0200

    feat: new props 'cut' and 'rotation'

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,8 +1,12 @@
-import React, { useContext } from 'react';
-import { PropsContext } from './Provider';
+import React, { useContext } from "react";
+import { PropsContext } from "./Provider";
 
 const Background = () => {
-  let { stroke, strokeBottom, colorCircle } = useContext(PropsContext);
+  let { stroke, round, inverse, strokeBottom, cut, rotation, colorCircle } =
+    useContext(PropsContext);
+
+  const dashoffset = 264 - (100 - cut) * 2.64;
+
   return (
     <circle
       cx="50"
@@ -10,8 +14,15 @@ const Background = () => {
       r="42"
       shapeRendering="geometricPrecision"
       fill="none"
+      style={{
+        transform: `rotate(${rotation}deg)`,
+        transformOrigin: "50% 50%"
+      }}
       stroke={colorCircle}
       strokeWidth={strokeBottom || stroke}
+      strokeLinecap={round ? "round" : "butt"}
+      strokeDashoffset={inverse ? -dashoffset : dashoffset}
+      strokeDasharray={264}
     />
   );
 };
