b84e40232f80ee3fc55ecce735a473d300e26a78
Grzegorz Tomicki <gtomik@gmail.com>
2022-01-03 00:26:58 +0100

    reafactor: accessibility update

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -2,14 +2,17 @@ import React, { useContext } from "react";
 import { PropsContext } from "./Provider";
 
 const Wrapper = ({ children }) => {
-  let { id, size } = useContext(PropsContext);
+  let { id, size, counter } = useContext(PropsContext);
   return (
     <svg
-      role="img"
+      role="progressbar"
       width={size}
       height={size}
       data-index={id}
       viewBox="0 0 100 100"
+      aria-valuemin={0}
+      aria-valuemax={100}
+      aria-valuenow={counter}
       aria-labelledby="circular progress bar">
       {children}
     </svg>
